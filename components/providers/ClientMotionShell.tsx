72309c79a257b5d7a0df5bc4a2085697c6f2b7cb
droidremmy76 <droidremmy@gmail.com>
2026-08-29 14:21:50 +0300

    Hoist scene and cursor providers into motion shell

--- components/providers/ClientMotionShell.tsx
+++ components/providers/ClientMotionShell.tsx
@@ -3,6 +3,9 @@
 import dynamic from "next/dynamic";
 import type { ReactNode } from "react";
 import { MotionProvider } from "@/context/MotionContext";
+import { SceneProvider } from "@/context/SceneContext";
+import { CursorProvider } from "@/context/CursorContext";
+import { CustomCursor } from "@/components/motion/CustomCursor";
 
 const PersistentThreeCanvas = dynamic(
   () => import("@/components/three/PersistentThreeCanvas").then((m) => m.PersistentThreeCanvas),
@@ -12,8 +15,13 @@ const PersistentThreeCanvas = dynamic(
 export function ClientMotionShell({ children }: { children: ReactNode }) {
   return (
     <MotionProvider>
-      {children}
-      <PersistentThreeCanvas />
+      <SceneProvider>
+        <CursorProvider>
+          <PersistentThreeCanvas />
+          {children}
+          <CustomCursor />
+        </CursorProvider>
+      </SceneProvider>
     </MotionProvider>
   );
 }
